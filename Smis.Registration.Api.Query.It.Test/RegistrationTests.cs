6f7d1913bb9c4770f07d1fa678455bc6a3781d45
mauro-fanton <mauro.fanton@opencastsoftware.com>
2023-06-16 15:51:52 +0100

    project first commit

---
+++ Smis.Registration.Api.Query.It.Test/RegistrationTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Smis.Registration.Api.Query.It.Test.Fixture;
+
+namespace Smis.Registration.Api.Query.It.Test;
+
+public class RegistrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+
+    public RegistrationTests(CustomWebApplicationFactory<Program> factory)
+    {
+        factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/smis/register/");
+
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
+
